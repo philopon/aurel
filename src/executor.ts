c8f9160c0cff044bb66349705f97cad6f14511dc
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2017-10-08 03:50:25 +0900

    skip succeeding commands when command failure

--- src/executor.ts
+++ src/executor.ts
@@ -22,6 +22,7 @@ export class Executor {
                 await command.execute();
             } catch (e) {
                 console.error(e);
+                break;
             }
         }
     }
